a0483d83ffa7c8e5b46d63fd1c462314bd669803
JovanaLazic <jovanalazic9696@gmail.com>
2017-04-09 21:18:07 +0200

    Popravljena dokumentacija

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,32 +1,33 @@
 package com.twitter.poruke;
 /**
- * Klasom TwitterPoruka se unose i vracaju atributi korisnik i poruka. Vraca se i njihova kombinacija u vidu toString metode
+ * Klasa predstavlja poruku na Twitteru
  * @author Jovana Lazic
  *
  */
 
 	public class TwitterPoruka {
 		/**
-		 * Atribut korisnik govori ko je osoba koja ostavlja poruku na Twitteru
+		 * Username korisnika.
 		 */
 		private String korisnik;
+		
 		/**
-		 * Atribut poruka sadzi tekst poruke koja se objavljuje
+		 * Tekst poruke.
 		 */
 		private String poruka;
 		
 		/**
-		 * 
-		 * @return String vrednost - ime korisnika
+		 * Vraca vrednost atributa korisnik.
+		 * @return username korisnika kao String
 		 */
 		public String getKorisnik() {
 			
 			return korisnik;
 	}
 		/**
-		 * Metoda setKorisnik postavlja vrednost atributa korisnik na onu vrednost koja se unosi kao parametar
-		 * @param korisnik Unosti se string koji predstavlja ime korisnika
-		 * @throws RuntimeException metoda baca izuzetak u slucaju da je unet prazan string ili null vrednost kao parametar
+		 * Postavlja vrednost atributa korisnik na onu vrednost koja se unosi kao parametar
+		 * @param korisnik novi username korisnika
+		 * @throws RuntimeException u slucaju da je unet prazan string ili null vrednost kao parametar
 		 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik==null || !korisnik.isEmpty())
@@ -36,7 +37,7 @@ package com.twitter.poruke;
 	}
 	
 	/**
-	 * 
+	 * Vraca vrednost atributa poruka.
 	 * @return String vrednost - tekst poruke
 	 */
 	public String getPoruka() {
@@ -44,9 +45,9 @@ package com.twitter.poruke;
 	}
 	
 	/**
-	 * Metoda setPoruka postavlja vrednost atributa poruka na onu vrednost koja se unosi kao parametar
-	 * @param poruka Unosi se string koji predstavlja tekst poruke
-	 * @throws RuntimeException metoda baca izuzetak u slucaju da je unet prazan string ili null vrednost kao parametar
+	 * Postavlja vrednost atributa poruka na onu vrednost koja se unosi kao parametar
+	 * @param poruka novi tekst poruke
+	 * @throws RuntimeException u slucaju da je unet prazan string ili null vrednost kao parametar
 	 */
 	public void setPoruka(String poruka) {
 		if (this.poruka==null || this.poruka == new String("") ||
@@ -55,8 +56,8 @@ package com.twitter.poruke;
 					"Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
-	/**
-	 * @return String vrednost koja predstavlja opis koji je korisnik uneo poruku i koji je tekst poruke
+	/**Vraca string vrednost sa podacima o korisniku i poruci
+	 * @return String username korisnika i tekst poruke uz odgovorajuci tekst
 	 */
 	public String toString(){
 	return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
